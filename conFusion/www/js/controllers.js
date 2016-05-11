03d63c5b08c79c5c1c84537642825b7473900787
Georges BODIONG <deebodiong@gmail.com>
2016-05-11 19:51:53 +0100

    lists and tabs
    Ionic lists, tabs and the grid in formatting and displaying information
    in the UI of our app

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -41,23 +41,68 @@ angular.module('conFusion.controllers', [])
   };
 })
 
+.controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
+
+            $scope.baseURL = baseURL;
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+            $scope.showMenu = false;
+            $scope.message = "Loading ...";
+
+            menuFactory.getDishes().query(
+                function(response) {
+                    $scope.dishes = response;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                });
+
+
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
 .controller('ContactController', ['$scope', function($scope) {
 
             $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-            
+
             var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-            
+
             $scope.channels = channels;
             $scope.invalidChannelSelection = false;
-                        
+
         }])
 
         .controller('FeedbackController', ['$scope', 'feedbackFactory', function($scope,feedbackFactory) {
-            
+
             $scope.sendFeedback = function() {
-                
+
                 console.log($scope.feedback);
-                
+
                 if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
@@ -73,12 +118,14 @@ angular.module('conFusion.controllers', [])
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-            
+        .controller('DishDetailController', ['$scope', '$stateParams',
+        'menuFactory', 'baseURL', function($scope, $stateParams, menuFactory, baseURL) {
+
+            $scope.baseURL = baseURL;
             $scope.dish = {};
             $scope.showDish = false;
             $scope.message="Loading ...";
-            
+
             $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
             .$promise.then(
                             function(response){
@@ -90,23 +137,23 @@ angular.module('conFusion.controllers', [])
                             }
             );
 
-            
+
         }])
 
         .controller('DishCommentController', ['$scope', 'menuFactory', function($scope,menuFactory) {
-            
+
             $scope.mycomment = {rating:5, comment:"", author:"", date:""};
-            
+
             $scope.submitComment = function () {
-                
+
                 $scope.mycomment.date = new Date().toISOString();
                 console.log($scope.mycomment);
-                
+
                 $scope.dish.comments.push($scope.mycomment);
         menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
-                
+
                 $scope.commentForm.$setPristine();
-                
+
                 $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             }
         }])
@@ -133,10 +180,10 @@ angular.module('conFusion.controllers', [])
       }])
 
         .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
-            
+
                     $scope.leaders = corporateFactory.query();
                     console.log($scope.leaders);
-            
+
                     }])
 
 ;
